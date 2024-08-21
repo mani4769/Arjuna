64dbd90a871c8d128e3b6e430dfb43d83f28f050
[mani4769] <[manikantathirumalla342@gmail.com]>
2024-08-21 18:12:23 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 //com
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
